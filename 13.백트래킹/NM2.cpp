0c0a65e930756fc1b0913a41a1c13bb1cebd3afc
yonguk <yonguk@yongukui-MacBookPro.local>
2022-03-28 01:53:27 +0900

    backtracking algorithm added

--- 13.백트래킹/NM2.cpp
+++ 13.백트래킹/NM2.cpp
@@ -1,26 +1,24 @@
 #include<iostream>
-#define MAX 8
+#define MAX 9
 using namespace std;
 
 int N,M,k;
 int arr[MAX] = {0, };
 bool visited[MAX] = {0, };
 
-void dfs(int count,int k){
+void dfs(int count, int k){
     if(count == M){
         for(int i = 0; i < M; i++){
-            cout << arr[i] << ' ';
-            
+            cout << arr[i]+1 << ' ';
         }
         cout << '\n';
         return;
     }
-
-    for(int i = 0; i < N; i++){
+    for(int i = k; i < N; i++){
         if(!visited[i]){
             visited[i] = true;
             arr[count] = i;
-            dfs(count+1,k);
+            dfs(count+1,i+1);
             visited[i] = false;
         }
     }
@@ -28,7 +26,7 @@ void dfs(int count,int k){
 
 int main(){
     cin >> N >> M;
-    int k = 0;
+    k = 0;
     dfs(0,k);
     
 }
